182188bbeb489863d076d912880ae0d4ea504bb8
jhin93 <scene1993@gmail.com>
2022-04-25 10:02:33 +0900

    getBytesDemo.sol 작성중 6편 3:20까지 시청

---
+++ artifacts/getBytesDemo.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT Liscense
+pragma solidity >=0.7.0 <0.9.0;
+
+contract getBytes {
+    function getBytesNow1() pure public returns (bytes32[1] memory ba1) {
+        //6편 3:20 까지 시청
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
