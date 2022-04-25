3c5736c0fc0e18fd4c8f046e13e3ad17c0dc66d2
jhin93 <scene1993@gmail.com>
2022-04-25 12:00:52 +0900

    7:10까지 봄

--- artifacts/getBytesDemo.sol
+++ artifacts/getBytesDemo.sol
@@ -2,8 +2,17 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract getBytes {
-    function getBytesNow1() pure public returns (bytes32[1] memory ba1) {
-        //6편 3:20 까지 시청
+    function getBytesNow1() pure public returns (bytes32[1] memory ba1) 
+    {
+        ba1 = [bytes32("Proposal A")];
+    }
+    function getBytesNow2() pure public returns (bytes32[1] memory ba2) 
+    {
+        ba2 = [bytes32("Proposal B")];
+    }
+    function getBytesNow3() pure public returns (bytes32[1] memory ba3) 
+    {
+        ba3 = [bytes32("Proposal C")];
     }
 }
 
